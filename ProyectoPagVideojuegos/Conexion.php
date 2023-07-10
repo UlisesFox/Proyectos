8bab9e516761f93568589960dfdd4daed146b0f2
Eluky <114705704+UlisesFox@users.noreply.github.com>
2023-07-10 13:19:34 -0600

    Update Conexion.php

--- ProyectoPagVideojuegos/Conexion.php
+++ ProyectoPagVideojuegos/Conexion.php
@@ -1,9 +1,9 @@
 <?php
 
 $db_host="localhost";
-$db_user="root";
+$db_user="uli";
 $db_name="registro";
-$db_password="";
+$db_password="1234";
 
 $connect = mysqli_connect($db_host,$db_user,$db_password,$db_name);
 
